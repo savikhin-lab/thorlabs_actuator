027e3f5fa07d12d770e382b8a2dd8fc1c0df8856
Zach Mitchell <zmitchell@fastmail.com>
2021-01-20 17:02:21 -0500

    fix: Hard code the DLL to load
    It's possible to load the wrong DLL if the path is not set explicitly.

--- etalon/actuator_low_level.py
+++ etalon/actuator_low_level.py
@@ -2,15 +2,16 @@ from ctypes import cdll, c_short, POINTER, c_char, c_int, c_bool, CDLL, CFUNCTYP
 
 
 def bind(lib, fn_name, argtypes=None, restypes=None):
-    """Create a Python wrapper around a C function.
-    """
+    """Create a Python wrapper around a C function."""
     _func = getattr(lib, fn_name)
     _func.argtypes = argtypes
     _func.restypes = restypes
     return _func
 
 
-lib = cdll.LoadLibrary("Thorlabs.MotionControl.KCube.DCServo.dll")
+lib = cdll.LoadLibrary(
+    "C:\\Program Files\\Thorlabs\\Kinesis\\Thorlabs.MotionControl.KCube.DCServo.dll"
+)
 
 # Setup and teardown
 TLI_BuildDeviceList = bind(lib, "TLI_BuildDeviceList", None, c_short)
@@ -23,13 +24,17 @@ CC_StopPolling = bind(lib, "CC_StopPolling", [POINTER(c_char)])
 # Homing
 CC_CanHome = bind(lib, "CC_CanHome", [POINTER(c_char)], c_bool)
 CC_NeedsHoming = bind(lib, "CC_NeedsHoming", [POINTER(c_char)], c_bool)
-CC_CanMoveWithoutHomingFirst = bind(lib, "CC_CanMoveWithoutHomingFirst", [POINTER(c_char)], c_bool)
+CC_CanMoveWithoutHomingFirst = bind(
+    lib, "CC_CanMoveWithoutHomingFirst", [POINTER(c_char)], c_bool
+)
 CC_Home = bind(lib, "CC_Home", [POINTER(c_char)], c_short)
 
 # Motion
 CC_GetPosition = bind(lib, "CC_GetPosition", [POINTER(c_char)], c_int)
 CC_MoveRelative = bind(lib, "CC_MoveRelative", [POINTER(c_char), c_int], c_short)
-CC_SetMoveAbsolutePosition = bind(lib, "CC_SetMoveAbsolutePosition", [POINTER(c_char), c_int], c_short)
+CC_SetMoveAbsolutePosition = bind(
+    lib, "CC_SetMoveAbsolutePosition", [POINTER(c_char), c_int], c_short
+)
 CC_MoveAbsolute = bind(lib, "CC_MoveAbsolute", [POINTER(c_char)], c_short)
 CC_StopImmediate = bind(lib, "CC_StopImmediate", [POINTER(c_char)], c_short)
 CC_StopProfiled = bind(lib, "CC_StopProfiled", [POINTER(c_char)], c_short)
